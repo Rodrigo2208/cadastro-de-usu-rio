926717ace35e79721f6662efa8db897b12c644f1
Rodrigo2208 <rodrigoduraes.nascimento11@gmail.com>
2024-12-02 17:31:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function carregarUsuarios() {
         const linha = document.createElement('tr');
         linha.innerHTML = `
             <td>${usuario.nome}</td>
-            <td>${usuario.email}</td>
+            <td>${usuario.quantidade}</td>
             <td class="actions">
                 <button class="edit-btn" onclick="editarUsuario(${index})"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
                 <button class="delete-btn" onclick="excluirUsuario(${index})"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
@@ -18,9 +18,9 @@ function carregarUsuarios() {
 }
 
 // Função para salvar um novo usuário
-function salvarUsuario(nome, email, senha) {
+function salvarUsuario(nome, quantidade, descricao) {
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-    usuarios.push({ nome, email, senha });
+    usuarios.push({ nome, quantidade, descricao });
     localStorage.setItem('usuarios', JSON.stringify(usuarios));
     carregarUsuarios();
 }
@@ -30,7 +30,7 @@ function editarUsuario(index) {
     const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
     const usuario = usuarios[index];
     document.getElementById('nome').value = usuario.nome;
-    document.getElementById('email').value = usuario.email;
+    document.getElementById('quantidade').value = usuario.quantidade;
 
     // Atualiza o botão de cadastro para salvar a edição
     const form = document.getElementById('formCadastro');
@@ -38,8 +38,8 @@ function editarUsuario(index) {
     form.addEventListener('submit', function salvarEdicao(e) {
         e.preventDefault();
         usuario.nome = document.getElementById('nome').value.trim();
-        usuario.email = document.getElementById('email').value.trim();
-        usuario.senha = document.getElementById('senha').value.trim();
+        usuario.quantidade = document.getElementById('quantidade').value.trim();
+        usuario.descricao = document.getElementById('descricao').value.trim();
         usuarios[index] = usuario;
         localStorage.setItem('usuarios', JSON.stringify(usuarios));
         carregarUsuarios();
@@ -61,11 +61,11 @@ function excluirUsuario(index) {
 function adicionarUsuario(e) {
     e.preventDefault(); // Impede o envio padrão do formulário
     const nome = document.getElementById('nome').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const senha = document.getElementById('senha').value.trim();
+    const quantidade = document.getElementById('quantidade').value.trim();
+    const descricao = document.getElementById('descricao').value.trim();
 
-    if (nome && email && senha) {
-        salvarUsuario(nome, email, senha);
+    if (nome && quantidade && descricao) {
+        salvarUsuario(nome, quantidade, descricao);
         document.getElementById('formCadastro').reset(); // Limpa o formulário
     }
 }
